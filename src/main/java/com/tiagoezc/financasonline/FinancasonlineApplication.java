80dd40f41932af0e3ec432d6acf555518130a2d5
tiagoeuzebiocordeiro <tiagoezc@hotmail.com>
2024-01-17 14:37:35 -0300

    Projeto criado

---
+++ src/main/java/com/tiagoezc/financasonline/FinancasonlineApplication.java
@@ -0,0 +1,13 @@
+package com.tiagoezc.financasonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancasonlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancasonlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiagoezc/financasonline/FinancasonlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiagoezc.financasonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancasonlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
